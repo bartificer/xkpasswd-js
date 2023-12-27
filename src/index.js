64f1b634fd06d513894373d2706b58bc8bddd58f
Helma van der Linden <hepabolu@gmail.com>
2023-12-27 11:23:25 +0100

    feat: #9 create a first version of the website with basic placeholders

---
+++ src/index.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+  // enable tooltips
+  const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]');
+  const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl));
+
+})
