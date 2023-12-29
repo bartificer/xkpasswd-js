a7662f0f9256eadd2ee60a89ddd0233b68384ae9
Helma van der Linden <hepabolu@gmail.com>
2023-12-29 12:05:58 +0100

    Maint: apply ESLint

--- src/index.mjs
+++ src/index.mjs
@@ -8,7 +8,7 @@
  */
 
 // import bootstrap plugins
-import { Tooltip } from 'bootstrap';
+import {Tooltip} from 'bootstrap';
 
 // import bootstrap CSS and icons
 import 'bootstrap/dist/css/bootstrap.min.css';
