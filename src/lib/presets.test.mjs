6797bbe7ccaa8bcc34f0af0b04e05e53309d65c2
Helma van der Linden <hepabolu@gmail.com>
2023-12-29 15:14:48 +0100

    Fix: add extra tests for Presets to create 100% coverage

--- src/lib/presets.test.mjs
+++ src/lib/presets.test.mjs
@@ -6,6 +6,30 @@ import {Presets} from './presets.mjs';
 
 describe('Test class Presets', () => {
   const me = Presets;
+
+  const mock = {
+    description: 'Mock preset',
+    config: {
+      // eslint-disable-next-line max-len
+      symbol_alphabet: ['.', ';'],
+      word_length_min: 4,
+      word_length_max: 8,
+      num_words: 1,
+      separator_character: 'RANDOM',
+      separator_alphabet: ['+', '-'],
+      padding_digits_before: 2,
+      padding_digits_after: 2,
+      padding_type: 'FIXED',
+      padding_character: 'SEPARATOR',
+      padding_alphabet: ['?', '!'],
+      padding_characters_before: 2,
+      padding_characters_after: 2,
+      case_transform: 'CAPITALIZE',
+      random_increment: 'AUTO',
+    },
+  };
+
+
   describe('Test function getDefault()', () => {
     /*
      * Because it is a static function, I don't think I need to initialize
@@ -18,6 +42,46 @@ describe('Test class Presets', () => {
 
       expect(typeof obj.config.num_words !== undefined).toBe(true);
       expect(obj.config.num_words).toBe(3);
+    });
+  });
+
+  describe('Test function getCurrent', () => {
+    const def = me.getDefault();
+
+    test('when initialized without parameter gives default set', () => {
+      const preset = (new Presets()).getCurrent();
+      expect(preset).toEqual(def);
+    });
+
+    test('when initialized with my preset gives the preset I added', () => {
+      const preset = new Presets(mock);
+
+      expect(preset.getCurrent()).not.toEqual(def);
+      expect(preset.getCurrent()).toEqual(mock);
+      expect(preset.name()).toBe('CUSTOM');
+    });
+  });
+
+  describe('Test function getSeparatorAlphabet', () => {
+    test('if separator_alphabet exists, it is chosen', () => {
+      const preset = new Presets(mock);
+      const def = new Presets();
+      const alfa = preset.config().separator_alphabet;
+      const alfaDef = def.config().symbol_alphabet;
+
+      expect(preset.getSeparatorAlphabet()).toEqual(alfa);
+      expect(def.getSeparatorAlphabet()).toEqual(alfaDef);
+    });
+  });
+  describe('Test function getPaddingAlphabet', () => {
+    test('if padding_alphabet exists, it is chosen', () => {
+      const preset = new Presets(mock);
+      const def = new Presets();
+      const alfa = preset.config().padding_alphabet;
+      const alfaDef = def.config().symbol_alphabet;
+
+      expect(preset.getPaddingAlphabet()).toEqual(alfa);
+      expect(def.getPaddingAlphabet()).toEqual(alfaDef);
     });
   });
 });
