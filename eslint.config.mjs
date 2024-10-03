442b2c5b437548a90f111385872d5bea33fbee27
Helma van der Linden <hepabolu@gmail.com>
2024-10-03 14:44:16 +0200

    MAINTENANCE update the ESLint configuration
    - remove old compatibility code
    - create a configuration just for the javascript files
    - add the new jsdoc plugin
    - add extra jsdoc rules
    
    Sadly they don't seem to work

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,36 +3,60 @@ import path from 'node:path';
 import {fileURLToPath} from 'node:url';
 import js from '@eslint/js';
 import {FlatCompat} from '@eslint/eslintrc';
-import jsdoc from "eslint-plugin-jsdoc";
+import jsdoc from 'eslint-plugin-jsdoc';
 import jest from 'eslint-plugin-jest';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-  recommendedConfig: js.configs.recommended,
-  allConfig: js.configs.all,
-});
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
+// const compat = new FlatCompat({
+//   baseDirectory: __dirname,
+//   recommendedConfig: js.configs.recommended,
+//   allConfig: js.configs.all,
+// });
 
-export default [...compat.extends('google'), {
-  plugins: {
-    jsdoc: jsdoc,
-    jest: jest,
-  },
-
-  languageOptions: {
-    globals: {
-      ...globals.browser,
-      ...jest.environments.globals.globals,
+export default [
+  // app related javascript files
+  {
+    name: 'app files',
+    files: ["src/**/*.mjs"],
+    ignores: ['src/**/*.test.mjs'],
+    plugins: {
+        jsdoc,
     },
 
-    ecmaVersion: 'latest',
-    sourceType: 'module',
+    ...jsdoc.configs['flat/recommended'],
+    settings: {
+      jsdoc: {
+        mode: 'jsdoc'
+      }
+    },
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+    },
+    rules: {
+      indent: ['error', 2],
+      'jsdoc/require-description': 'error',
+      'jsdoc/check-values': 'error',
+      'jsdoc/informative-docs': 1,
   },
 
-  rules: {
-    indent: ['error', 2],
-    "jsdoc/require-description": "error",
-    "jsdoc/check-values": "error"
-  },
+  // // ...compat.extends('google'),
+  // {
+  // plugins: {
+  //   jsdoc,
+  //   jest,
+  // },
+  //
+  //
+  // languageOptions: {
+  //   globals: {
+  //     ...globals.browser,
+  //     ...globals.jest,
+  //   },
+  //
+  //   ecmaVersion: 'latest',
+  //   sourceType: 'module',
+  // },
+  //
 }];
